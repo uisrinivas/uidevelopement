0c78d8382e2cc3723c44ee3e726bce8908a56d18
Srinivas <uisrinivas@gmail.com>
2016-03-18 12:59:57 +0530

    adding the gulp-sass
    adding the scss file support

--- gulpfile.js
+++ gulpfile.js
@@ -2,16 +2,23 @@ var gulp = require('gulp');
 var sourcemaps = require('gulp-sourcemaps');
 var babel = require('gulp-babel');
 var concat = require('gulp-concat');
+var sass = require('gulp-sass');
 
 gulp.task('js', function () {
 	return gulp.src('public/js/**/*.js')
-		.pipe(sourcemaps.init())
-		.pipe(babel({presets: ['es2015']}))
-		.pipe(concat('public/js/all.js'))
-		.pipe(sourcemaps.write('.'))
-		.pipe(gulp.dest('dist/js'));
+	.pipe(sourcemaps.init())
+	.pipe(babel({presets: ['es2015']}))
+	.pipe(concat('public/js/all.js'))
+	.pipe(sourcemaps.write('.'))
+	.pipe(gulp.dest('dist/js'));
 });
 
-gulp.task('default', ["js"]);
+gulp.task('sass', function () {
+  return gulp.src('public/css/scss/**/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('public/css'));
+});
+
+gulp.task('default', ["js",'sass']);
 
 
