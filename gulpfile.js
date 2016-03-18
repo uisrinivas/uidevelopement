0b813a1cc445d832571883b59987469cc90870fc
Srinivas <uisrinivas@gmail.com>
2016-03-18 11:41:45 +0530

    updated the server
    adding the live-server need to improve the  server

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,3 @@
-'use strict';
-
 var gulp = require('gulp');
 var sourcemaps = require('gulp-sourcemaps');
 var babel = require('gulp-babel');
@@ -9,7 +7,7 @@ gulp.task('js', function () {
 	return gulp.src('public/js/**/*.js')
 		.pipe(sourcemaps.init())
 		.pipe(babel({presets: ['es2015']}))
-		.pipe(concat('public/js/**/all.js'))
+		.pipe(concat('public/js/all.js'))
 		.pipe(sourcemaps.write('.'))
 		.pipe(gulp.dest('dist/js'));
 });
